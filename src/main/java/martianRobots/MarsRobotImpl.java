b24d81e285c7d4542dcff21dd64c8226ed78a298
RyanDur <ryan.p.durling@gmail.com>
2014-11-21 01:16:22 +0000

    refactor to setters an getters

--- src/main/java/martianRobots/MarsRobotImpl.java
+++ src/main/java/martianRobots/MarsRobotImpl.java
@@ -9,17 +9,18 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.*;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.joining;
 import static martianRobots.lang.Constants.*;
 
 public class MarsRobotImpl implements MarsRobot {
-    private boolean lost;
-    private char orientation;
-    private List<Integer> pos;
+    private Supplier<String> lost;
+    private Supplier<Character> orientation;
+    private Supplier<Integer> x;
+    private Supplier<Integer> y;
     private Set<List<Integer>> scents;
-    private BiPredicate<Integer, Integer> outOfBounds;
+    private BiPredicate<Integer, Integer> isOutOfBounds;
 
     public MarsRobotImpl() {
         scents = new HashSet<>();
@@ -27,67 +28,73 @@ public class MarsRobotImpl implements MarsRobot {
 
     @Override
     public void setup(int x, int y) throws InvalidGridSizeException {
-        if (invalidSize.test(x, y)) throw new InvalidGridSizeException(x, y);
-        setBoundary.accept(x, y);
+        if (isInvalidSize.test(x, y)) throw new InvalidGridSizeException(x, y);
+        setBounds.accept(x, y);
     }
 
     @Override
     public void move(String instructions) throws InvalidInstructions {
-        if (invalid.test(instructions)) throw new InvalidInstructions(instructions);
-        for (int i = 0; i < instructions.length() && !lost; i++) execute(instructions.charAt(i));
+        if (isInvalid.test(instructions)) throw new InvalidInstructions(instructions);
+        for (int i = 0; i < instructions.length() && !lost.get().equals(LOST); i++)
+            execute(instructions.charAt(i));
     }
 
     @Override
-    public void setPosition(int x, int y, char orientation) throws InvalidMoveException {
-        if (outOfBounds.test(x, y) || invalidCompass.test(orientation))
-            throw new InvalidMoveException(x, y, orientation);
-        lost = false;
-        this.orientation = orientation;
-        pos = Arrays.asList(x, y);
+    public void setPosition(int x, int y, char direction) throws InvalidMoveException {
+        if (isOutOfBounds.test(x, y) || !COMPASS.contains(direction)) throw new InvalidMoveException(x, y, direction);
+        setLost.accept("");
+        setOrientation.accept(direction);
+        setCoordinates(x, y);
     }
 
     @Override
     public String getPosition() {
-        return Stream.of(pos.get(0), pos.get(1), orientation, lost ? LOST : "")
-                .map(word -> word.toString()).collect(Collectors.joining(" ")).trim();
+        return Stream.of(x, y, orientation, lost).map(word -> word.get().toString()).collect(joining(" ")).trim();
     }
 
     private void execute(char direction) {
         if (direction == FORWARD) {
-            List<Integer> newPos = moveForward(pos.get(0), pos.get(1), orientation);
+            List<Integer> newPos = moveForward(x.get(), y.get());
             if (!scents.contains(newPos)) {
-                if (outOfBounds.test(newPos.get(0), newPos.get(1))) {
-                    lost = true;
+                if (isOutOfBounds.test(newPos.get(0), newPos.get(1))) {
+                    setLost.accept(LOST);
                     scents.add(newPos);
-                } else pos = newPos;
+                } else setCoordinates(newPos.get(0), newPos.get(1));
             }
-        } else orientation = turn(direction, orientation);
+        } else setOrientation.accept(turn(direction));
     }
 
-    private char turn(char direction, char orientation) {
-        int index = COMPASS.indexOf(orientation);
+    private char turn(char direction) {
+        int index = COMPASS.indexOf(orientation.get());
         return COMPASS.get(direction == RIGHT ?
                 index + 1 > COMPASS.size() - 1 ? 0 : index + 1 :
                 index - 1 < 0 ? COMPASS.size() - 1 : index - 1);
     }
 
-    private List<Integer> moveForward(int x, int y, char orientation) {
-        if (orientation == NORTH) y += 1;
-        else if (orientation == SOUTH) y -= 1;
-        else if (orientation == EAST) x += 1;
+    private List<Integer> moveForward(int x, int y) {
+        if (orientation.get() == NORTH) y += 1;
+        else if (orientation.get() == SOUTH) y -= 1;
+        else if (orientation.get() == EAST) x += 1;
         else x -= 1;
         return Arrays.asList(x, y);
     }
 
-    private BiFunction<Integer, Integer, BiPredicate<Integer, Integer>> setValidation = (maxX, maxY) ->
+    private void setCoordinates(int x, int y) {
+        this.x = () -> x;
+        this.y = () -> y;
+    }
+
+    private Consumer<String> setLost = message -> lost = () -> message;
+
+    private Consumer<Character> setOrientation = orientation -> this.orientation = () -> orientation;
+
+    private BiFunction<Integer, Integer, BiPredicate<Integer, Integer>> getBoundary = (maxX, maxY) ->
             (x, y) -> x < 0 || x > maxX || y < 0 || y > maxY;
 
-    private BiConsumer<Integer, Integer> setBoundary = (x, y) -> outOfBounds = setValidation.apply(x, y);
+    private BiConsumer<Integer, Integer> setBounds = (x, y) -> isOutOfBounds = getBoundary.apply(x, y);
 
-    private Predicate<Character> invalidCompass = orientation -> !COMPASS.contains(orientation);
+    private BiPredicate<Integer, Integer> isInvalidSize = (x, y) -> getBoundary.apply(MAX_SIZE, MAX_SIZE).test(x, y);
 
-    private BiPredicate<Integer, Integer> invalidSize = (x, y) -> setValidation.apply(MAX_SIZE, MAX_SIZE).test(x, y);
-
-    private Predicate<String> invalid = instructions -> instructions.length() >= MAX_INSTRUCTION_SIZE ||
+    private Predicate<String> isInvalid = instructions -> instructions.length() >= MAX_INSTRUCTION_SIZE ||
             instructions.chars().filter(c -> c != LEFT && c != RIGHT && c != FORWARD).count() > 0;
 }

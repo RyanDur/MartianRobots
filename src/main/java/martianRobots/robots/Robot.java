60853c8a3ace31a9ddca70558b0bdf5ea5874535
RyanDur <ryan.p.durling@gmail.com>
2014-11-25 23:40:21 +0000

    information on how to extend the instructions for the robot

--- src/main/java/martianRobots/robots/Robot.java
+++ src/main/java/martianRobots/robots/Robot.java
@@ -9,6 +9,10 @@ import java.util.List;
  * A robot to traverse the surface of mars.
  * A robots position consists of a grid coordinate (a pair of integers: x-coordinate followed by y-coordinate)
  * and a Compass orientation (N, S, E, W for north, south, east, and west).
+ * To create a new Instruction for the robot, just create a new class named by a single character
+ * (e.g. B, as in backwards) and extend it from RobotImpl. There is no need for any other configuration, RobotImpl
+ * will automatically pick it up if it comes across the instruction. For an example please refer to the other
+ * instruction classes F, L, or R.
  */
 public interface Robot {
 
